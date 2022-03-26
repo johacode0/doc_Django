7c2819fe59c41ceb69e574182fc5595fc57e6350
el kachabi <badreddinelkachabi@gmail.com>
2022-03-26 19:09:03 +0100

    Écriture de votre première application Django, 7ème partie Personnalisation du formulaire d’administration Ajout d’objets liés Personnalisation de la liste pour modification de l’interface d’administration Personnalisation de l’apparence de l’interface d’administration Personnalisation des gabarits de votre projet Personnalisation des gabarits de votre application Personnalisation de la page d’accueil de l’interface d’administration Et ensuite ?

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
